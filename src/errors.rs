6acae2d703532b3d4fc6490909f90ac92fc6a6fb
Cyrielle <cyriellecentori@protonmail.com>
2024-09-25 13:47:20 +0200

    Annulation de la refonte de errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,6 @@
 //! Définit la structure d’erreur utilisée par la bibliothèque. Contient de nombreuses erreurs
 //! spécifiques à fondabots en plus de la retransmission d’erreurs des bibliothèques utilisées.
 use std::fmt::{Debug, Display, Formatter};
-use std::time::SystemTimeError;
 
 /// Objet d’erreur utilisé par fondabots.
 ///
@@ -13,10 +12,10 @@ pub enum Error {
     ObjectNotFound(String),
     /// Un conteneur est vide alors qu’il n’aurait pas dû l’être.
     EmptyContainer(String),
-    SerenityError(serenity::Error),
+    /*SerenityError(serenity::Error),
     YamlEmitError(yaml_rust2::EmitError),
     IOError(std::io::Error),
-    SystemTimeError(SystemTimeError),
+    SystemTimeError(SystemTimeError),*/
     /// Erreur de lecture de Yaml.
     YamlParseError(String),
     /// Identifiant d’interaction invalide en comparaison de ce qui était attendu. Contient
@@ -34,7 +33,8 @@ pub enum Error {
     /// exister (condition préalable vérifiée en amont mais indication de l’erreur obligatoire
     /// par exemple). En général, l’utilisation de ce type d’erreur est le signe d’un mauvais
     /// code, mais cela peut être utile de passer par là en première instance.
-    Generic
+    Generic,
+    LibError(Box<dyn std::error::Error + Sync + Send + 'static>)
 }
 
 impl Display for Error {
@@ -42,16 +42,17 @@ impl Display for Error {
         match self {
             Error::ObjectNotFound(e) => write!(f, "Objet non trouvé : {e}"),
             Error::EmptyContainer(e) => write!(f, "Conteneur vide : {e}"),
-            Error::SerenityError(e) => Display::fmt(&e, f),
+            /*Error::SerenityError(e) => Display::fmt(&e, f),
             Error::YamlEmitError(e) => Display::fmt(&e, f),
             Error::IOError(e) => Display::fmt(&e, f),
-            Error::SystemTimeError(e) => Display::fmt(&e, f),
+            Error::SystemTimeError(e) => Display::fmt(&e, f),*/
             Error::YamlParseError(e) => write!(f, "Erreur de formatage yaml : {e}"),
             Error::InteractionIDError(id, message) => write!(f, "Erreur de format de l’identifiant {id} sur le message {message}"),
             Error::NoneError => write!(f, "Option None non-attendue."),
             Error::UnloadedItem(id) => write!(f, "Affichan {id} appelé mais non chargé."),
             Error::Generic => write!(f, "Erreur de bot générique."),
             Error::CommandUseError(e) => write!(f, "Erreur d’utilisation de la commande : {e}"),
+            Error::LibError(e) => Display::fmt(&e, f)
         }
     }
 }
@@ -62,7 +63,7 @@ unsafe impl Send for Error {}
 
 unsafe impl Sync for Error  {}
 
-impl std::error::Error for Error {}
+/* impl std::error::Error for Error {} // Pour la 2.0.0
 
 impl From<serenity::Error> for Error {
     fn from(e: serenity::Error) -> Self {Error::SerenityError(e)}
@@ -77,5 +78,20 @@ impl From<yaml_rust2::EmitError> for Error {
 }
 
 impl From<SystemTimeError> for Error {
-    fn from(e: SystemTimeError) -> Self {Error::SystemTimeError(e)}
+    fn from(e: SystemTimeError) -> Self { Error::SystemTimeError(e) }
+}
+
++ Ajouter une fonction pour encapsuler un autre type d’erreur, mais je ne peux pas implémenter
+From<std::error::Error> si l’objet Erreur d’ici implémente lui-même std::error::Error. L’idée serait
+de créer un trait qui ajoute une fonction à Result<T, E: std::error::Error>. Ou peut-être juste
+régulariser tout ça et créer un type d’erreur renvoyé pour chaque fonction, qui permet de
+lister toutes les erreurs que peut renvoyer la fonction ? Réflexion à suivre, pour le moment
+on garde le système peut-être un peu bancal.
+
+*/
+
+impl<E: std::error::Error + Sync + Send + 'static> From<E> for Error {
+    fn from(value: E) -> Self {
+        Error::LibError(Box::new(value))
+    }
 }
